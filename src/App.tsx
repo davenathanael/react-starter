5b34c598fdaaac34be6cfa99c176d6f62632b928
Dave Nathanael <davenathanaeldev@gmail.com>
2020-08-29 20:30:28 +0700

    feat: configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-green-200">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
